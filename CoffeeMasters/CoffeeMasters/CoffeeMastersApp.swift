dff526204e25719b0d5b514cdac145b901a4e405
Sahar <snbabaee@gmail.com>
2025-02-01 13:00:25 -0800

    finished swift UI overview

---
+++ CoffeeMasters/CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,73 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Sahar Babaei on 2025-02-01.
+//
+
+// this code is a structure, not a class!
+// option+click on the word "App" you see it is a protocol.
+// so we're implementing a protocol.
+
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    //an app needs a body, therefore this body variable :
+    // on ipad you can run more than 1 app on the screen at the same time, not on iphone
+    var body: some Scene { //a scene is mostly used in ipads, as an app window
+        WindowGroup {
+            ContentView() //content view is the other file we have on the left.
+        }
+    }
+}
+
+
+
+//---> Regarding classes, structures and "some"
+/*
+// --> super class
+class Base{
+
+}
+
+// --> class A extends Base
+class A:Base{
+    
+}
+
+// --> class B extends Base
+class B:Base{
+    
+}
+
+// --> when we want a function to return an object of the subclass A
+func test()-> A{
+    return A()
+}
+
+// --> when we want a function to return an object of either subclass A or B
+func test(something: Int)-> Base{
+    if something == 0{
+        return A()
+    }
+    else{
+        return B()
+    }
+}
+
+
+// --> but what is "some"?
+// this some says that you can return A or B, but it either has to be always A or always B, or always Base
+func test(something: Int)-> some Base{
+    if something == 0{
+        return A()
+    }
+    else{
+        //return B()
+        return A()
+    }
+}
+*/
+
---
+++ CoffeeMasters/CoffeeMastersTests/CoffeeMastersTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersTests.swift
+//  CoffeeMastersTests
+//
+//  Created by Sahar Babaei on 2025-02-01.
+//
+
+import Testing
+@testable import CoffeeMasters
+
+struct CoffeeMastersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
